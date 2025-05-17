3d095c4a9b16e5526dd7a5767f1dd2d2d1818535
George Sakellaropoulos <sboygeorge@hotmail.com>
2025-05-17 15:43:46 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+const Card = () => {
+    return (
+        <div className="card">
+            <h1>Card Component</h1>
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+    )
+}
+
+
+export default App

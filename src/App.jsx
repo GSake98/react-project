85f7890aeda3c691a1524cd2538d2a8afa2d3483
George Sakellaropoulos <sboygeorge@hotmail.com>
2025-05-20 10:41:05 +0300

    Add background and search placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import {useTranslation} from "react-i18next";
+import React, {useState} from 'react'
+import {Trans} from "react-i18next";
+import Search from "./components/search.jsx";
 
 const App = () => {
-    const {t} = useTranslation();
-
+    const [searchTerm, setSearchTerm] = useState('');
     return (
         <main>
             <div className="pattern"></div>
             <div className="wrapper">
                 <header>
-                    <h1>{t('headline')}</h1>
+                    <img src="./hero.png" alt="Hero Banner"/>
+                    <h1>
+                        <Trans i18nKey="headline" components={{ gradient: <span className="text-gradient" /> }}/>
+                    </h1>
                 </header>
+
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                <h1 className="text-white">{searchTerm}</h1>
             </div>
         </main>
     )
